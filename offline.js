2457dd451cbff0d0babedd823365465b42190b6c
semicon <30399464+semicon@users.noreply.github.com>
2019-08-09 15:33:22 +0700

    savedata
    savedata

---
+++ offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1565339447,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/next-sheet0.png",
+		"images/home-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
